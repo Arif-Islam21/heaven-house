97a19c0883f70b75b89934d62907efd3bab58dc2
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-04-21 16:35:40 +0600

    register page design has done and started the banner section

--- src/Routes/Root.jsx
+++ src/Routes/Root.jsx
@@ -1,11 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Components/Navbar/Navbar";
+import Footer from "../Components/Footer/Footer";
 
 const Root = () => {
   return (
-    <div>
-      <Navbar></Navbar>
-      <Outlet></Outlet>
+    <div className="flex flex-col min-h-screen">
+      <div className="flex-1">
+        <Navbar></Navbar>
+        <Outlet></Outlet>
+      </div>
+      <div className="">
+        <Footer></Footer>
+      </div>
     </div>
   );
 };
