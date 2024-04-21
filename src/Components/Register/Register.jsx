68f34e1c605dcf8a5ddc48ffcd7a6577631a4908
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-04-21 16:24:29 +0600

    register page added

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,7 +1,74 @@
+import { NavLink } from "react-router-dom";
+
 const Register = () => {
   return (
     <div>
-      <h2>Register Now</h2>
+      <div className="hero min-h-full bg-base-100">
+        <div className="hero-content">
+          <div className="card shrink-0 w-full px-12 py-6 shadow-2xl bg-base-100">
+            <form className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Your Name"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Photo Url</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Photo Url"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                  required
+                />
+                <label className="label label-text-alt link link-hover">
+                  <p>
+                    Already have an account?{" "}
+                    <NavLink
+                      to={"/login"}
+                      className="text-blue-600 font-semibold"
+                    >
+                      LogIn
+                    </NavLink>{" "}
+                  </p>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Register</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
