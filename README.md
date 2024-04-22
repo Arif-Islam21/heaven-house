d0bc3d47300e6d52b37793be9fe98e563b53d146
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-04-22 09:31:38 +0600

    working with the slider

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 JSON data link: https://arif-islam21.github.io/recidential-json-data/recidential.json
+
+Image links:
+https://ibb.co/fYhS2Pd
+https://ibb.co/qN9vVFF
+https://ibb.co/Bc7Sy8m
+https://ibb.co/qW2DPyx
+https://ibb.co/D9G2bcd
+https://ibb.co/YRZMHnX
+https://ibb.co/xDz3F97
--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,30 +1,42 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import { Navigation, Pagination } from "swiper/modules";
 
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
+import { useEffect, useState } from "react";
 
 const Banner = () => {
+  const [estateData, setEstateData] = useState([]);
 
-    const swiper = new Swiper(...);
+  useEffect(() => {
+    // fetch(
+    //   "https://arif-islam21.github.io/recidential-json-data/recidential.json"
+    // )
+    fetch("../../../public/recidential.json")
+      .then((res) => res.json())
+      .then((data) => setEstateData(data));
+  }, []);
+
+  console.log(estateData);
   return (
     <Swiper
       // install Swiper modules
-      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      modules={[Navigation, Pagination]}
       spaceBetween={50}
-      slidesPerView={3}
+      slidesPerView={1}
       navigation
       pagination={{ clickable: true }}
       scrollbar={{ draggable: true }}
       onSwiper={(swiper) => console.log(swiper)}
       onSlideChange={() => console.log("slide change")}
     >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
+      {estateData.map((estate) => (
+        <SwiperSlide key={estate.id}>
+          <img src={estate.image} alt={estate.description} />
+        </SwiperSlide>
+      ))}
       ...
     </Swiper>
   );
