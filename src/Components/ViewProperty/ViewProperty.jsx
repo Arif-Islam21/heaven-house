1839efdb2ca66d0efbbd7b6d1f70b1967bae850f
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-04-23 16:31:35 +0600

    design skiped

--- src/Components/ViewProperty/ViewProperty.jsx
+++ src/Components/ViewProperty/ViewProperty.jsx
@@ -21,63 +21,22 @@ const ViewProperty = () => {
 
   return (
     <div>
-      <section className=" container mx-auto my-12">
-        <div className="hero min-h-[85vh] ">
-          <div className="hero-content  flex-col lg:flex-row">
-            <div className="w-1/2 bg-base-200 py-36 flex min-h-full justify-center items-center">
-              <img src={image} className="max-w-sm rounded-lg shadow-2xl" />
-            </div>
-            <div className="w-1/2 pl-8">
-              <h1 className="text-2xl lg:text-5xl font-bold">bookName</h1>
-              <p className="text-md font-medium my-3">By:author</p>
-              <hr />
-              <p className="my-3 font-medium">category</p>
-              <hr />
-              <p className="font-medium text-md my-3 text-gray-500">
-                <span className="text-xl text-black font-bold">Review : </span>
-                review
-              </p>
-              <p className="flex flex-col lg:flex-row gap-4 items-center my-3">
-                <span className="text-xl font-bold">Tag:</span>
-                <span className="flex gap-3">
-                  {/* {tags.map((tag, idx) => (
-                    <span
-                      className="text-green-500 font-semibold px-2 py-1 bg-green-100 my-1 rounded-md"
-                      key={idx}
-                    >
-                      #{tag}
-                    </span>
-                  ))} */}
-                  tags map here
-                </span>
-              </p>
-              <hr />
-              <div className="flex flex-col lg:flex-row items-center text-start my-3">
-                <p className="font-semibold w-full lg:w-1/2 text-gray-500">
-                  Number Of Pages:
-                </p>
-                <p className="font-semibold w-1/2 text-black">totalPages</p>
-              </div>
-              <div className="flex flex-col lg:flex-row items-center my-3">
-                <p className="font-semibold w-1/2 text-gray-500">Publisher:</p>
-                <p className="font-semibold w-1/2 text-black">publisher</p>
-              </div>
-              <div className="flex flex-col lg:flex-row items-center  my-3">
-                <p className="font-semibold w-1/2 text-gray-500">
-                  Year Of Publication
-                </p>
-                <p className="font-semibold w-1/2 text-black">
-                  yearOfPublishing
-                </p>
-              </div>
-              <div className="flex items-center my-3">
-                <p className="font-semibold w-1/2 text-gray-500">Rating</p>
-                <p className="font-semibold w-1/2 text-black">rating</p>
-              </div>
-            </div>
+      <div
+        className="hero min-h-screen "
+        style={{
+          backgroundImage: `url(${image})`,
+        }}
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 lg:text-4xl md:text-4xl  font-bold">
+              {segment_name}
+            </h1>
+            <p className="mb-5">{description}</p>
           </div>
         </div>
-      </section>
+      </div>
     </div>
   );
 };
