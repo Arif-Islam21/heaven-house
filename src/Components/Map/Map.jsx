241dce478bccdf7b6747a6e823407fa64c192b1d
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-04-26 06:21:00 +0600

    react leaflet map done

--- src/Components/Map/Map.jsx
+++ src/Components/Map/Map.jsx
@@ -1,30 +1,24 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 function Map() {
-  const position = [51.505, -0.09];
-
   return (
-    <div
-      style={{
-        position: "fixed",
-        top: "0",
-        left: "0",
-        right: "0",
-        bottom: "0",
-      }}
+    <MapContainer
+      className="border-2 h-[60vh] w-[60vw] mx-auto"
+      center={[25.891639, 88.900512]}
+      zoom={13}
+      scrollWheelZoom={false}
     >
-      <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
-    </div>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
   );
 }
 
