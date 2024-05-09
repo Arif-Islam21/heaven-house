f40957a429e6c0cbb7c030ae4aa9ed1d8f69b65e
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-05-09 23:34:15 +0530

    environment variable setup done

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -4,14 +4,15 @@ import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+console.log(import.meta.env.VITE_APIKEY);
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAQqZG5KX71TelXcm_-PmPL7O8_qTYYj_Q",
-  authDomain: "heaven-house-2.firebaseapp.com",
-  projectId: "heaven-house-2",
-  storageBucket: "heaven-house-2.appspot.com",
-  messagingSenderId: "81160091160",
-  appId: "1:81160091160:web:fe6fca14d66c52258c356d",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSENGING_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
